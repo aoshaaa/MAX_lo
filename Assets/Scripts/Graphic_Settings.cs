1a34b0128f930fd9ab0cacab8e56b542fabd05d9
aoshaaa <kalachev.artemm@gmail.com>
2020-10-20 20:29:18 +0300

    20.10

---
+++ Assets/Scripts/Graphic_Settings.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Graphic_Settings : MonoBehaviour
+{
+           
+   
+
+    public void setGraphic(int graphicIndex)
+    {
+         QualitySettings.SetQualityLevel(graphicIndex);
+        int textureQuality = graphicIndex;
+        PlayerPrefs.SetInt("TextureQuality", textureQuality);
+        PlayerPrefs.Save();        
+    }
+
+    public void setFullScreen(bool isFullScreen)
+    {
+        if (isFullScreen)
+        {
+            Resolution[] allResolutions = Screen.resolutions;
+            Resolution maxResolution = allResolutions[allResolutions.Length - 1];
+            Screen.SetResolution(maxResolution.width, maxResolution.height, true);
+            Debug.Log("full");
+        }
+    }
+}
---
+++ Assets/Scripts/Quality_Dropdown.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Dropdown))]
+public class Quality_Dropdown : MonoBehaviour
+{
+    Dropdown _dropdown;
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Start()
+    {
+        _dropdown = GetComponent<Dropdown>();
+        _dropdown.value = PlayerPrefs.GetInt("TextureQuality");
+    }
+
+    private void Update()
+    {
+        
+        Debug.Log(_dropdown.value);
+    }
+}
